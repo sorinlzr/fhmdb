a38b08171ce8533a40bc86cc5d7e7b668a2a05f8
Burak Kongo <burak.kongo@stud.fh-campuswien.ac.at>
2023-02-28 16:33:07 +0100

    PROG2-8: Implemented the search functionality into the controller class

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -1,34 +1,36 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.features.MovieSearchService;
 import at.ac.fhcampuswien.fhmdb.models.Genre;
 import at.ac.fhcampuswien.fhmdb.models.Movie;
-import at.ac.fhcampuswien.fhmdb.service.MovieFilterService;
 import at.ac.fhcampuswien.fhmdb.ui.MovieCell;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXListView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Set;
 
 public class HomeController implements Initializable {
     @FXML
-    public JFXButton resetFilterBtn;
+    public JFXButton searchBtn;
 
     @FXML
     public TextField searchField;
 
     @FXML
-    public JFXListView<Movie> movieListView;
+    public JFXListView movieListView;
 
     @FXML
-    public JFXComboBox<Genre> genreComboBox;
+    public JFXComboBox genreComboBox;
 
     @FXML
     public JFXButton sortBtn;
@@ -36,36 +38,54 @@ public class HomeController implements Initializable {
     public List<Movie> allMovies = Movie.initializeMovies();
 
     private final ObservableList<Movie> observableMovies = FXCollections.observableArrayList();   // automatically updates corresponding UI elements when underlying data changes
-    private final FilteredList<Movie> filteredList = new FilteredList<>(observableMovies);
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         observableMovies.addAll(allMovies);         // add dummy data to observable list
 
         // initialize UI stuff
-        movieListView.setItems(filteredList);   // set data of observable list to list view
+        movieListView.setItems(observableMovies);   // set data of observable list to list view
         movieListView.setCellFactory(movieListView -> new MovieCell()); // use custom cell factory to display data
 
         genreComboBox.setPromptText("Filter by Genre");
         genreComboBox.getItems().addAll(Genre.values());
 
+        // TODO add event handlers to buttons and call the regarding methods
+        // either set event handlers in the fxml file (onAction) or add them here
+
+        searchField.setOnKeyTyped(keyEvent -> {
+            String searchTerm = searchField.getText().trim();
+            searchKeyword(searchTerm);
+        });
+
+
         // Sort button example:
         sortBtn.setOnAction(actionEvent -> {
-            if(sortBtn.getText().equals("Sort (asc)")) {
-                observableMovies.sort(Comparator.naturalOrder());
+            if (sortBtn.getText().equals("Sort (asc)")) {
+                // TODO sort observableMovies ascending
                 sortBtn.setText("Sort (desc)");
             } else {
-                observableMovies.sort(Collections.reverseOrder());
+                // TODO sort observableMovies descending
                 sortBtn.setText("Sort (asc)");
             }
         });
+
+
     }
 
-    public void resetFilterCriteria() {
-        MovieFilterService.resetFilterCriteria(genreComboBox, filteredList, searchField);
-    }
-
-    public void selectGenre() {
-        MovieFilterService.selectSpecificGenre(genreComboBox, filteredList);
+    /**
+     * This method performs a search operation on a list of movies based on a given search term.
+     * It updates the observableMovies list with the search results.
+     *
+     * @param searchTerm The keyword to search in the movie titles and descriptions.
+     */
+    private void searchKeyword(String searchTerm) {
+        observableMovies.setAll(allMovies);
+        MovieSearchService movieSearchService = new MovieSearchService(observableMovies);
+        Set<Movie> searchResults = new HashSet<>();
+        searchResults.addAll(movieSearchService.searchInMovieTitle(searchTerm));
+        searchResults.addAll(movieSearchService.searchInMovieDescription(searchTerm));
+        observableMovies.clear();
+        observableMovies.addAll(searchResults);
     }
 }
