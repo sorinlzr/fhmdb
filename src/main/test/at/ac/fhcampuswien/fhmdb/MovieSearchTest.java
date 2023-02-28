93b3bbd986746e7abcb95bc35283fa414a7e9c7c
Burak Kongo <burak.kongo@stud.fh-campuswien.ac.at>
2023-02-28 16:33:38 +0100

    PROG2-8: set up the testing file for the search functionality

---
+++ src/main/test/at/ac/fhcampuswien/fhmdb/MovieSearchTest.java
@@ -0,0 +1,56 @@
+package at.ac.fhcampuswien.fhmdb;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import at.ac.fhcampuswien.fhmdb.features.MovieSearch;
+
+public class MovieSearchTest {
+
+    /*
+        1.Test:
+        Verification that the search functionality works properly by entering a valid search term.
+
+        2.Test:
+        Verification that the search functionality works properly when multiple search terms are used.
+
+        3.Test:
+        Verification that the search functionality works properly when the search query is empty.
+
+        4.Test:
+        Verification that the search functionality works properly by entering an invalid search term.
+
+        5.Test:
+        Verification that the search functionality works properly by entering a search term with multiple words.
+
+        6.Test:
+        Verification that the search functionality works properly by entering a search term with special characters.
+
+        7.Test:
+        Verification that the search functionality works properly by entering a search term with leading and trailing spaces.
+
+        8.Test:
+        Verification that the search functionality works properly when the search query is empty.
+
+        9.Test:
+        Verification that the search functionality works properly when there are multiple results.
+
+        10.Test:
+        Verification that the search functionality works properly when the search query is misspelled.
+
+        11.Test:
+        Verification that the search functionality works properly by entering a search term with uppercase letters.
+
+        12.Test:
+        Verification that the search functionality works properly by entering a search term with lowercase letters.
+
+        13.Test:
+        Verification that the search functionality works properly by entering a search term with a mix of upper- and lower-case letters.
+
+     */
+
+
+}
