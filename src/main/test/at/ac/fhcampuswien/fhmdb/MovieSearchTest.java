d8a0daddb1232b11ab10cbffaaefa1057282770a
Burak Kongo <burak.kongo@stud.fh-campuswien.ac.at>
2023-03-01 19:57:44 +0100

    PROG2-8: Added different test cases to test the functionalities

--- src/main/test/at/ac/fhcampuswien/fhmdb/MovieSearchTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/MovieSearchTest.java
@@ -1,56 +1,149 @@
 package at.ac.fhcampuswien.fhmdb;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import at.ac.fhcampuswien.fhmdb.features.MovieSearchService;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
 
-import at.ac.fhcampuswien.fhmdb.features.MovieSearch;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MovieSearchTest {
+class MovieSearchTest {
 
-    /*
-        1.Test:
-        Verification that the search functionality works properly by entering a valid search term.
+    private final List<Movie> movies = Movie.initializeMovies();
+    MovieSearchService movieSearchService = new MovieSearchService(movies);
 
-        2.Test:
-        Verification that the search functionality works properly when multiple search terms are used.
+    @Test
+    void SearchInMovieTitleWithValidTerm() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("Harry Potter");
+        assertEquals(1, result.size());
+    }
 
-        3.Test:
-        Verification that the search functionality works properly when the search query is empty.
-
-        4.Test:
-        Verification that the search functionality works properly by entering an invalid search term.
-
-        5.Test:
-        Verification that the search functionality works properly by entering a search term with multiple words.
-
-        6.Test:
-        Verification that the search functionality works properly by entering a search term with special characters.
-
-        7.Test:
-        Verification that the search functionality works properly by entering a search term with leading and trailing spaces.
-
-        8.Test:
-        Verification that the search functionality works properly when the search query is empty.
-
-        9.Test:
-        Verification that the search functionality works properly when there are multiple results.
-
-        10.Test:
-        Verification that the search functionality works properly when the search query is misspelled.
-
-        11.Test:
-        Verification that the search functionality works properly by entering a search term with uppercase letters.
-
-        12.Test:
-        Verification that the search functionality works properly by entering a search term with lowercase letters.
-
-        13.Test:
-        Verification that the search functionality works properly by entering a search term with a mix of upper- and lower-case letters.
-
-     */
+    @Test
+    void SearchInMovieDescriptionWithValidTerm() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("nose");
+        assertEquals(1, result.size());
+    }
 
 
+    @Test
+    void SearchInMovieTitleWithMultipleTerms() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("Star Wars: Episode");
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithMultipleTerms() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("without a nose");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithEmptyQuery() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("");
+        assertEquals(result.size(), result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithEmptyQuery() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("");
+        assertEquals(result.size(), result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithInvalidTerm() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("Xyz");
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithInvalidTerm() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("Xyz");
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithSpecialChars() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("$#@!%");
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithSpecialChars() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("$#@!%");
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithLeadingTrailingSpaces() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("  Titanic  ");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithLeadingTrailingSpaces() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("  nose  ");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithMultipleResults() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("Star Wars");
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithMultipleResults() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("movie");
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithMisspelledQuery() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("Harri Pottr");
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithMisspelledQuery() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("bqat");
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithUppercaseQuery() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("TITANIC");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithUppercaseQuery() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("BOAT");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithLowercaseQuery() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("titanic");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void SearchInMovieTitleWithMixedCaseQuery() {
+        List<Movie> result = movieSearchService.searchInMovieTitle("HaRRy pOTTer");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void SearchInMovieDescriptionWithMixedCaseQuery() {
+        List<Movie> result = movieSearchService.searchInMovieDescription("bOaT");
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void IfResultAppearsOnlyOnceInTheSetOfResults() {
+        Set<Movie> result = movieSearchService.searchForMovie("die");
+        assertEquals(1, result.size());
+    }
 }
