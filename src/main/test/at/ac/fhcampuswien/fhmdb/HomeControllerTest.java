03b62e94fd3f6dfd440ed1d945a5ac8dca921904
sorin.lzr <soso.laz@gmail.com>
2023-03-08 19:05:21 +0100

    PROG2-16: add test stubs for the search and filter by genre functionality

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,7 +1,59 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
+
+    @Nested
+    public class GenreIsSelected {
+        @Test
+        public void whenGenreIsSelected_thenFilteredListShouldContainOnlyMoviesWithThatGenre() {
+            fail("Not implemented yet");
+        }
+    }
+
+    @Nested
+    public class SearchFieldIsFilled {
+        @Test
+        public void whenSearchFieldIsFilled_thenFilteredListShouldContainOnlyMoviesThatMatchSearchCriteria() {
+            fail("Not implemented yet");
+        }
+    }
+
+    @Nested
+    public class GenreIsSelectedAndSearchFieldIsFilled {
+        @Test
+        public void filteredListShouldContainOnlyMoviesThatMatchSearchCriteriaAndGenre() {
+            fail("Not implemented yet");
+        }
+
+        @Test
+        public void whenGenreIsReset_thenFilteredListShouldContainOnlyMoviesThatMatchSearchCriteria() {
+            fail("Not implemented yet");
+        }
+
+        @Test
+        public void whenSearchFieldIsReset_thenFilteredListShouldContainOnlyMoviesThatMatchGenre() {
+            fail("Not implemented yet");
+        }
+
+        @Test
+        public void SearchFieldIsResetAndThenGenreIsReset_thenFilteredListShouldContainAllMovies() {
+            fail("Not implemented yet");
+        }
+
+        @Test
+        public void SearchFieldIsResetAndThenGenreIsResetAndThenGenreIsSelectedAgain_thenFilteredListShouldContainOnlyMoviesThatMatchGenre() {
+            fail("Not implemented yet");
+        }
+
+        @Test
+        public void SearchFieldIsResetAndThenGenreIsResetAndThenSearchFieldIsFilledAgain_thenFilteredListShouldContainOnlyMoviesThatMatchSearchCriteria() {
+            fail("Not implemented yet");
+        }
+    }
 
 }
