131ffdfadc1dd14d54dcf3fad5cba9a2ba408ed1
Benjamin Lichtenstein <31620900+bennilic@users.noreply.github.com>
2023-05-01 14:33:46 +0200

    PROG2-25: Adds information to the movie cells. Changes UI display.

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -2,23 +2,16 @@ package at.ac.fhcampuswien.fhmdb.ui;
 
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 import java.io.IOException;
-import java.util.List;
 
 public class MovieCell extends ListCell<Movie> {
-    private Node graphic;
-    private MovieCellController controller;
+    private final Node graphic;
+    private final MovieCellController controller;
 
-    {
+    public MovieCell() {
         try {
             FXMLLoader loader = new FXMLLoader(MovieCell.class.getResource("movie-cell.fxml"));
             graphic = loader.load();
@@ -37,66 +30,16 @@ public class MovieCell extends ListCell<Movie> {
             setGraphic(null);
         } else {
             controller.setTitle(movie.getTitle());
+            controller.setDescription(movie.getDescription());
+            controller.setGenre(movie.getGenres());
+            controller.setYear(movie.getReleaseYear());
+            controller.setRuntime(movie.getLengthInMinutes());
+            controller.setRating(movie.getRating());
+            controller.setWriters(movie.getWriters());
+            controller.setDirectors(movie.getDirectors());
+            controller.setMainCast(movie.getMainCast());
             setGraphic(graphic);
         }
     }
-
-//    private static final String TEXT_WHITE = "text-white";
-//    private static final String TEXT_YELLOW = "text-yellow";
-//    private final Label title = new Label();
-//    private final Label detail = new Label();
-//    private final Label genre = new Label();
-//    private final Label movieDetails = new Label();
-//    private final Label producingCast = new Label();
-//    private final Label directors = new Label();
-//    private final Label mainCast = new Label();
-//    private final VBox layout = new VBox(title, detail, genre, movieDetails, producingCast, mainCast);
-//
-//    @Override
-//    protected void updateItem(Movie movie, boolean empty) {
-//        super.updateItem(movie, empty);
-//
-//        if (empty || movie == null) {
-//            setText(null);
-//            setGraphic(null);
-//        } else {
-//            this.getStyleClass().add("movie-cell");
-//            title.setText(movie.getTitle());
-//            detail.setText(
-//                    movie.getDescription() != null
-//                            ? movie.getDescription()
-//                            : "No description available"
-//            );
-//
-//            genre.setText(movie.getGenres().stream().map(Enum::name).sorted().reduce((a, b) -> a + ", " + b).orElse(""));
-//            genre.getStyleClass().add("text-genre");
-//            movieDetails.setText("Year: " + movie.getReleaseYear() + " • Runtime: " + movie.getLengthInMinutes() + " min • Rating: " + movie.getRating());
-//            producingCast.setText("Writers: " + formatListOfNames(movie.getWriters())
-//                            + " • Directors: " + formatListOfNames(movie.getDirectors()));
-//            mainCast.setText("Main Cast: " + formatListOfNames(movie.getMainCast()));
-//
-//            // color scheme
-//            title.getStyleClass().add(TEXT_YELLOW);
-//            detail.getStyleClass().add(TEXT_WHITE);
-//            movieDetails.getStyleClass().add(TEXT_WHITE);
-//            producingCast.getStyleClass().add(TEXT_WHITE);
-//            directors.getStyleClass().add(TEXT_WHITE);
-//            mainCast.getStyleClass().add(TEXT_WHITE);
-//            layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
-//
-//            // layout
-//            title.fontProperty().set(title.getFont().font(20));
-//            detail.setMaxWidth(this.getScene().getWidth() - 30);
-//            detail.setWrapText(true);
-//            layout.setPadding(new Insets(10));
-//            layout.spacingProperty().set(10);
-//            layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
-//            setGraphic(layout);
-//        }
-//    }
-//
-//    private String formatListOfNames(List<String> list) {
-//        return list.stream().sorted().reduce((a, b) -> a + ", " + b).orElse("");
-//    }
 }
 
--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCellController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCellController.java
@@ -1,13 +1,73 @@
 package at.ac.fhcampuswien.fhmdb.ui;
 
+import at.ac.fhcampuswien.fhmdb.filter.Genre;
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.util.List;
 
 public class MovieCellController {
     @FXML
     private Label title;
+    @FXML
+    private Label description;
+    @FXML
+    private Label genre;
+    @FXML
+    private Label year;
+    @FXML
+    private Label runtime;
+    @FXML
+    private Label rating;
+    @FXML
+    private Label writers;
+    @FXML
+    private Label directors;
+    @FXML
+    private Label mainCast;
+    @FXML
+    private JFXButton detailsButton;
+    @FXML
+    private JFXButton watchlistButton;
 
     public void setTitle(String title) {
         this.title.setText(title);
+    }
+
+    public void setDescription(String description) {
+        this.description.setText(description);
+    }
+
+    public void setGenre(List<Genre> genres) {
+        this.genre.setText(genres.stream().map(Enum::name).sorted().reduce((a, b) -> a + ", " + b).orElse(""));
+    }
+
+    public void setYear(int year) {
+        this.year.setText("Year: " + year);
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime.setText("Runtime: " + runtime + " min");
+    }
+
+    public void setRating(double rating) {
+        this.rating.setText("Rating: " + rating);
+    }
+
+    public void setWriters(List<String> writers) {
+        this.writers.setText("Writers: " + formatListOfNames(writers));
+    }
+
+    public void setDirectors(List<String> directors) {
+        this.directors.setText("Directors: " + formatListOfNames(directors));
+    }
+
+    public void setMainCast(List<String> mainCast) {
+        this.mainCast.setText("Main Cast: " + formatListOfNames(mainCast));
+    }
+
+    private String formatListOfNames(List<String> list) {
+        return list.stream().sorted().reduce((a, b) -> a + ", " + b).orElse("");
     }
 }
